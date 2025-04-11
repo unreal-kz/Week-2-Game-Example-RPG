3653492601390f2f358a0a07a1040bfae7206c64
unreal-kz <ukibas.b@gmail.com>
2025-04-11 08:37:21 +0500

    NPCs.java file added and updated

---
+++ NPCs.java
@@ -0,0 +1,10 @@
+public class NPCs {
+    private String name;
+    private String type;
+
+    public NPCs(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
+
+}
